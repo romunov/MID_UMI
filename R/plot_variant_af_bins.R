3b0690b196eee9391acbdf18878ae499954a39e6
Roman Luštrik <romunov@gmail.com>
2018-09-24 15:35:36 +0200

    Improved plotting of variant_afbins

--- R/plot_variant_af_bins.R
+++ R/plot_variant_af_bins.R
@@ -1,8 +1,32 @@
+#!/usr/bin/env Rscript
+
+# This script will plot output from SWIFT UMI pipeline.
+# Input file is variant_AFbins_prePOST-MID.txt. Run the script using e.g.
+# Rscript ./R/plot_variant_af_bins.R --input variant_AFbins_prePOST-MID.txt
+
 library(tidyr)
-library(ggplot2)
+library(ggplot2) # use dev version >= 3.0.0.9000 or bear the extra Rplots.pdf
+library(optparse)
+
+option.list <- list(
+  make_option(c("-i", "--input"), type = "character", default = NULL,
+              help = "Input file, normally variant_AFbins_prePOST-MID from
+                      the pipeline"),
+  make_option(c("-o", "--output"), type = "character", default = "variant_AFbins_prePOST-MID",
+              help = "Relative or absolute path to the output figure."),
+  make_option(c("--width"), type = "double", default = 10.0,
+              help = "Width of the output plot, in cm."),
+  make_option(c("--height"), type = "double", default = 10.0,
+              help = "Height of the output plot, in cm."),
+  make_option(c("-d", "--device"), type = "character", default = "pdf",
+              help = "Which device to use for plotting of the final image. Default is
+                        pdf, but e.g. png and jpg are also available. See ?ggsave for more
+                        info."))
+
+opt <- parse_args(OptionParser(option_list = option.list))
 
 # Import data and reflow it into a long format which is handy for plotting.
-xy <- read.table("../variant_AFbins_prePOST-MID.txt", header = TRUE, sep = "\t")
+xy <- read.table(opt$input, header = TRUE, sep = "\t")
 xy <- droplevels(xy[-1, ]) # remove first line because it's a summary
 
 # Save this to specify the correct order of levels.
@@ -20,7 +44,7 @@ xy <- xy[, c("vars", "sample", "origin", "value")]
 # Calculate summary stats per sample and origin.
 xy.stats <- aggregate(value ~ origin + sample, FUN = sum, data = xy)
 
-ggplot(xy, aes(x = vars, y = value, fill = origin)) +
+out <- ggplot(xy, aes(x = vars, y = value, fill = origin)) +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
         legend.position = "top") +
@@ -29,4 +53,5 @@ ggplot(xy, aes(x = vars, y = value, fill = origin)) +
   xlab("Vars AF") + ylab("Count") +
   facet_wrap(~ sample, ncol = 5)
 
-ggsave("../variant_AFbins_prePOST-MID.pdf", width = 6, height = 6)
+ggsave(paste(opt$output, opt$dev, sep = "."), 
+       width = opt$width, height = opt$height, device = opt$dev, units = "cm")
